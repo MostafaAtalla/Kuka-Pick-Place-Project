9dcc6af7bdcb4dae48fd6ab35c14e90d9b66a851
Mostafa Atalla <mostafa.a.atalla@gmail.com>
2020-04-11 03:10:27 -0400

    Adding the HT corresponding to the pose

--- kuka_arm/scripts/IK_server.py
+++ kuka_arm/scripts/IK_server.py
@@ -61,40 +61,46 @@ def handle_calculate_IK(req):
             (roll, pitch, yaw) = tf.transformations.euler_from_quaternion(
                     [req.poses[x].orientation.x, req.poses[x].orientation.y,
                     req.poses[x].orientation.z, req.poses[x].orientation.w])
+            # Assemble a Rotation matrix corresponding to those euler angles   
+            T_gripper = tf.transformations.euler_matrix(roll,pitch,yaw)
+            # Cast the rotation matrix to be sympy rotation matrix
+            T_gripper = Matrix(T_gripper)
+            # Assemble the homogenous transformation matrix corresponding to the pose
+            T_gripper[:3,3] = Matrix([px,py,pz])
 
-            ### Your IK code here
-	    # Calculate the wrist center position vector
-	    directional_vector = Matrix([1,0,0])   # directional vector from gripper frame origin to the wrist center
 
-        p_gripper = Matrix([px,py,pz])         # gripper frame origin position vector
+                ### Your IK code here
+            # Calculate the wrist center position vector
+            directional_vector = Matrix([1,0,0])   # directional vector from gripper frame origin to the wrist center      
 
-        p_wc = p_gripper - 0.303*directional_vector  # wrist center position vector
-	    
-	    # Calculate joint angles using Geometric IK method
+            p_wc = T_gripper[:3,3] - 0.303*directional_vector  # wrist center position vector
+            
+            # Calculate joint angles using Geometric IK method
 
-        # Calculate the inverse position solution
-	    # calculate first joint angle
-        theta1 = atan2(p_wc[1],p_wc[0])
-        # calculate second joint angle
-        a = 1.501
-        b = ((((p_wc[0]**2+p_wc[1]**2)**0.5) - 0.35)**2 + (p_wc[2]-0.75)**2)**0.5
-        c = 1.25
+            # Calculate the inverse position solution
+            # calculate first joint angle
+            theta1 = atan2(p_wc[1],p_wc[0])
+            # calculate second joint angle
+            a = 1.501
+            b = ((((p_wc[0]**2+p_wc[1]**2)**0.5) - 0.35)**2 + (p_wc[2]-0.75)**2)**0.5
+            c = 1.25
 
-        angle_a = acos((b**2+c**2-a**2)/(2*b*c))
+            angle_a = acos((b**2+c**2-a**2)/(2*b*c))
 
-        angle_b = acos((a**2+c**2-b**2)/(2*a*c))
+            angle_b = acos((a**2+c**2-b**2)/(2*a*c))
 
-        theta2 = pi/2 - angle_a - atan2(p_wc[2]-0.75,((p_wc[0]**2+p_wc[1]**2)**0.5)-0.35) 
-	    # calculate third joint angle
-        theta3 = pi/2 - (angle_b+0.036)
+            theta2 = pi/2 - angle_a - atan2(p_wc[2]-0.75,((p_wc[0]**2+p_wc[1]**2)**0.5)-0.35) 
+            # calculate third joint angle
+            theta3 = pi/2 - (angle_b+0.036)
 
-        
-            ###
+            # Calculate the inverse Orientation Solution
+            
+                ###
 
-            # Populate response for the IK request
-            # In the next line replace theta1,theta2...,theta6 by your joint angle variables
-	    joint_trajectory_point.positions = [theta1, theta2, theta3, theta4, theta5, theta6]
-	    joint_trajectory_list.append(joint_trajectory_point)
+                # Populate response for the IK request
+                # In the next line replace theta1,theta2...,theta6 by your joint angle variables
+            joint_trajectory_point.positions = [theta1, theta2, theta3, theta4, theta5, theta6]
+            joint_trajectory_list.append(joint_trajectory_point)
 
         rospy.loginfo("length of Joint Trajectory List: %s" % len(joint_trajectory_list))
         return CalculateIKResponse(joint_trajectory_list)
