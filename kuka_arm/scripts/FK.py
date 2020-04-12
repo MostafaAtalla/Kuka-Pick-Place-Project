b60dfe941215a13152ea6507f86fd7b8d847aab9
Mostafa Atalla <mostafa.a.atalla@gmail.com>
2020-04-11 23:14:39 -0400

    Modify debug to be a function with call section

--- kuka_arm/scripts/FK.py
+++ kuka_arm/scripts/FK.py
@@ -10,7 +10,14 @@ Key: 'q'     (joint angles)  - Value: list of joint angles
 It produces the following outputs:
 List of Sympy transformation matrices from base to gripper frame each w.r.t. the prior frame
 
-Dependacies: Sympy
+Function Dependacies: Sympy
+
+Debugging function is attached after the end of the function to test it
+to use debug function within this script, uncomment the debug function call section 
+and enter values for q,t and quat and run the script
+for more information about the debug function inputs please refer to the function and
+its call section
+ 
 '''
 from sympy import *
 import tf
@@ -41,75 +48,83 @@ def forward_kinematics(DH):
    
 
 ############################### Debugging Section ###############################################
+
+def debug(q,t,quat):
+
+    ######## Calculate Pose Using Function Herein ############
+    # Create Symbolic Variables
+    q1, q2, q3, q4, q5, q6, q7 = symbols('q1:8')       
+    # Create Modified DH parameters dictionary
+    DH = {'alpha': [0, -pi / 2, 0, -pi / 2, pi / 2, -pi / 2, 0],
+                'a': [0, 0.35, 1.25, -0.054, 0, 0, 0],
+                'd': [0.75, 0, 0, 1.5, 0, 0, 0.303],
+                'q': [q1, q2 - pi / 2, q3, q4, q5, q6,0]}
+    # Define the correction transformation between the gripper DH frame and gripper URDF frame
+    # Create the gripper frame correction matrix to align the DH frames with the URDF Frames
+    # The rotation matrix of this transformations is the unit vectors of the URDF gripper frame
+    # With respect to the DH gripper frame. The translation component is zero
+    Tdh_urdf = Matrix([[0,  0,   1,  0],
+                        [0, -1,   0,  0],
+                        [1,  0,   0,  0],
+                        [0,  0,   0,  1]])
+
+    # Call forward kinematics function
+    transforms = forward_kinematics(DH)
+    # Append the transformation matrix between the DH gripper frame and the URDF gripper frame
+    transforms.append(Tdh_urdf)
+        
+    # Create the total transforms from each frame to the base frame
+    T0_gripper = transforms[0]   # Transformation matrix from the URDF gripper frame to the base frame
+        
+    for i in range(1,len(transforms)):
+        T0_gripper = T0_gripper*transforms[i]     # Accumulating the transformations
+
+    # Calcuate the pose 4*4 matrix of the robot calculated through the developed forward kinematics function
+    # Joint values dictionary
+    # Enter Joint Values set in Joint State Publisher
+
+    values={}
+    values[q1] = -2.91
+    values[q2] = -0.78
+    values[q3] =  0.25
+    values[q4] =  5.19
+    values[q5] =  0.36
+    values[q6] =  3.57
+
+    T0_gripper = T0_gripper.evalf(subs=values)
+
+    ######## Calculate Pose Matrix Using RVIZ ACTUAL VALUES ############
+    # Create translation vector
+    # Enter translation vector valuesof gripper_link from RVIZ
+    translation = np.array([t])
+
+    # Create orienatation quaternion
+    # Enter orientation quaternion values of gripper_link from RVIZ
+    quaternion = quat
+
+    # Create Translation Matrix
+    T0_gripper_actual_translation = tf.transformations.translation_matrix(translation)
+    # Create Rotation Matrix
+    T0_gripper_actual_rotation = tf.transformations.quaternion_matrix(quaternion)
+    # Calculate Total Transformation and Cast it into Sympy Matrix
+    T0_gripper_actual = Matrix(np.dot(T0_gripper_actual_translation,T0_gripper_actual_rotation))
+
+
+    ######## Calculating and Printing Pose Error ############
+    pprint(T0_gripper_actual-T0_gripper)
+
 '''
-# Remove the comment quotation marks from here and at the end of this section to start debugging
+To use the debugging function to test the forward kinematics function
+uncomment this test section and enter the q, t and quat values
+# Debugging Function Inputs
+q = [-2.91,-0.78,0.25,5.19,0.36,3.57]         # list of joint values in radians
 
-######## Calculate Pose Using Function Herein ############
-# Create Symbolic Variables
-q1, q2, q3, q4, q5, q6, q7 = symbols('q1:8')       
-# Create Modified DH parameters dictionary
-DH = {'alpha': [0, -pi / 2, 0, -pi / 2, pi / 2, -pi / 2, 0],
-              'a': [0, 0.35, 1.25, -0.054, 0, 0, 0],
-              'd': [0.75, 0, 0, 1.5, 0, 0, 0.303],
-              'q': [q1, q2 - pi / 2, q3, q4, q5, q6,0]}
-# Define the correction transformation between the gripper DH frame and gripper URDF frame
-# Create the gripper frame correction matrix to align the DH frames with the URDF Frames
-# The rotation matrix of this transformations is the unit vectors of the URDF gripper frame
-# With respect to the DH gripper frame. The translation component is zero
-Tdh_urdf = Matrix([[0,  0,   1,  0],
-                       [0, -1,   0,  0],
-                       [1,  0,   0,  0],
-                       [0,  0,   0,  1]])
+t = [-1.0606, -0.15275, 2.4486]               # list of translation vector entries of the gripper URDF frame w.r.t base (obtained from RVIZ)
 
-# Call forward kinematics function
-transforms = forward_kinematics(DH)
-# Append the transformation matrix between the DH gripper frame and the URDF gripper frame
-transforms.append(Tdh_urdf)
-    
-# Create the total transforms from each frame to the base frame
-T0_gripper = transforms[0]   # Transformation matrix from the URDF gripper frame to the base frame
-    
-for i in range(1,len(transforms)):
-    T0_gripper = T0_gripper*transforms[i]     # Accumulating the transformations
+quat =  [0.094633, 0.94858, 0.264, -0.14678]  # list of quaternion values of the gripper URDF frame w.r.t base (obtained from RVIZ)
 
-# Calcuate the pose 4*4 matrix of the robot calculated through the developed forward kinematics function
-# Joint values dictionary
-# Enter Joint Values set in Joint State Publisher
-
-values={}
-values[q1] = -2.91
-values[q2] = -0.78
-values[q3] =  0.25
-values[q4] =  5.19
-values[q5] =  0.36
-values[q6] =  3.57
-
-T0_gripper = T0_gripper.evalf(subs=values)
-
-######## Calculate Pose Matrix Using RVIZ ACTUAL VALUES ############
-# Create translation vector
-# Enter translation vector valuesof gripper_link from RVIZ
-translation = np.array([[-1.0606, -0.15275, 2.4486]])
-
-# Create orienatation quaternion
-# Enter orientation quaternion values of gripper_link from RVIZ
-quaternion = [0.094633, 0.94858, 0.264, -0.14678]
-
-# Create Translation Matrix
-T0_gripper_actual_translation = tf.transformations.translation_matrix(translation)
-# Create Rotation Matrix
-T0_gripper_actual_rotation = tf.transformations.quaternion_matrix(quaternion)
-# Calculate Total Transformation and Cast it into Sympy Matrix
-T0_gripper_actual = Matrix(np.dot(T0_gripper_actual_translation,T0_gripper_actual_rotation))
-
-
-######## Calculating and Printing Pose Error ############
-pprint(T0_gripper_actual-T0_gripper)
-
-# Remove Quotation Marks Below and at the Beginning of This Section to Start Debugging
+debug(q,t,quat)
 '''
-
-
 
 
 
